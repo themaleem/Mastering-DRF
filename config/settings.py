ab4f3aaa59041f61536726cd06afcba0e8355e0e
themaleem <themaleem@gmail.com>
2020-03-26 15:38:41 +0100

    added pagination to API endpoints

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     # 3rd party
     'rest_framework',
+    "django_filters",
     
 ]
 
@@ -135,3 +136,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'drones.custompagination.LimitOffsetPaginationWithUpperBound',
+    'PAGE_SIZE': 4,
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
+        ),
+}
---
+++ drones/custompagination.py
@@ -0,0 +1,5 @@
+from rest_framework.pagination import LimitOffsetPagination
+
+class LimitOffsetPaginationWithUpperBound(LimitOffsetPagination):
+    # Set the maximum limit value to 8
+    max_limit = 8
