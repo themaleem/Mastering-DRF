16235858f5062f04dd7c6a91514e3795a2b035e8
themaleem <themaleem@gmail.com>
2020-03-29 21:56:19 +0100

    added docstring comments to API views

--- drones/views.py
+++ drones/views.py
@@ -10,6 +10,13 @@ from django_filters import AllValuesFilter, DateTimeFilter, NumberFilter
 from django_filters.rest_framework import FilterSet #using this instead of rest_framework.filters.FilterSet
 
 class DroneCategoryList(generics.ListCreateAPIView):
+    """
+    Return a list of all the drone categories that 
+    present within the queryset, with optional filtering.
+    ?search=<search-text>
+    &ordering=<ordeing keyr>
+    &filter=<>
+    """
     queryset = DroneCategory.objects.all()
     serializer_class = DroneCategorySerializer
     name = 'dronecategory-list'
@@ -23,22 +30,29 @@ class DroneCategoryList(generics.ListCreateAPIView):
         'name',
         )
 
-
 class DroneCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = DroneCategory.objects.all()
     serializer_class = DroneCategorySerializer
     name = 'dronecategory-detail'
 
-
 class DroneList(generics.ListCreateAPIView):
+    """
+    Return a list of all the drones that 
+    present within the queryset, with optional filtering.
+    ?search=<search-text>
+    &ordering=<ordering key>
+    &<any of the filtering_fields key below>
+
+    ie 127.0.0.1:8000/drones/?drone-category=1
+    """
     queryset = Drone.objects.all()
     serializer_class = DroneSerializer
     name = 'drone-list'
     filter_fields=(
-        'name',
-        'drone_category',
-        'manufacturing_date',
-        'has_it_competed',
+        'name', #name of the drone
+        'drone_category', #id of drone category
+        'manufacturing_date', #manufacturing datetime delta of the drone
+        'has_it_competed', #bool value 
         )
     search_fields=(
         '^name',
@@ -48,14 +62,21 @@ class DroneList(generics.ListCreateAPIView):
         'manufacturing_date',
         )
 
-
 class DroneDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Drone.objects.all()
     serializer_class = DroneSerializer
     name = 'drone-detail'
 
-
 class PilotList(generics.ListCreateAPIView):
+    """
+    Return a list of all the pilots that is 
+    present within the queryset, with optional filtering.
+    ?search=<search-text>
+    &ordering=<ordering key>
+    &<any of the filtering_fields key below>
+
+    ie 127.0.0.1:8000/drones/?name=
+    """
     queryset = Pilot.objects.all()
     serializer_class = PilotSerializer
     name = 'pilot-list'
@@ -72,32 +93,31 @@ class PilotList(generics.ListCreateAPIView):
         'race_count',
         )
 
-
 class PilotDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Pilot.objects.all()
     serializer_class = PilotSerializer
     name = 'pilot-detail'
 
-class CompetitionFilter(FilterSet):
-    from_achievement_date = DateTimeFilter(name='distance_achievement_date', lookup_expr='gte')
-    to_achievement_date = DateTimeFilter(name='distance_achievement_date', lookup_expr='lte')
-    min_distance_in_feet = NumberFilter(name='distance_in_feet', lookup_expr='gte')
-    max_distance_in_feet = NumberFilter(name='distance_in_feet', lookup_expr='lte')
-    drone_name = AllValuesFilter(name='drone__name')
-    pilot_name = AllValuesFilter(name='pilot__name')
-    class Meta:
-        model = Competition
-        fields = (
-            'distance_in_feet',
-            'from_achievement_date',
-            'to_achievement_date',
-            'min_distance_in_feet',
-            'max_distance_in_feet',
-            # drone__name will be accessed as drone_name
-            'drone_name',
-            # pilot__name will be accessed as pilot_name
-            'pilot_name',
-            )
+# class CompetitionFilter(FilterSet):
+#     from_achievement_date = DateTimeFilter(name='distance_achievement_date', lookup_expr='gte')
+#     to_achievement_date = DateTimeFilter(name='distance_achievement_date', lookup_expr='lte')
+#     min_distance_in_feet = NumberFilter(name='distance_in_feet', lookup_expr='gte')
+#     max_distance_in_feet = NumberFilter(name='distance_in_feet', lookup_expr='lte')
+#     drone_name = AllValuesFilter(name='drone__name')
+#     pilot_name = AllValuesFilter(name='pilot__name')
+#     class Meta:
+#         model = Competition
+#         fields = (
+#             'distance_in_feet',
+#             'from_achievement_date',
+#             'to_achievement_date',
+#             'min_distance_in_feet',
+#             'max_distance_in_feet',
+#             # drone__name will be accessed as drone_name
+#             'drone_name',
+#             # pilot__name will be accessed as pilot_name
+#             'pilot_name',
+#             )
 
 class CompetitionList(generics.ListCreateAPIView):
     queryset = Competition.objects.all()
@@ -108,7 +128,6 @@ class CompetitionList(generics.ListCreateAPIView):
     #     'distance_in_feet',
     #     'distance_achievement_date',
     #     )
-
 
 class CompetitionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Competition.objects.all()
